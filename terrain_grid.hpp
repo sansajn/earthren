9f37be0db4e8881f40c06eeb145df66804a00bcb
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2024-12-03 17:34:13 +0100

    terrain_grid moved to header file

---
+++ terrain_grid.hpp
@@ -0,0 +1,56 @@
+#include <ranges>
+#include <vector>
+#include <glm/vec2.hpp>
+#include <GLES3/gl32.h>
+
+/* - we are expecting that all terrains has the same size textures so no reason to store texture w/h
+- grid_size is also the same for all terrain */
+struct terrain {
+	GLuint elevation_map,
+		satellite_map;
+	glm::vec2 position;  //!< Terrain word position (within thee grid).
+	float elevation_min;  // TODO: use terrain related value there, TODO: rename to eelevation_max
+};
+
+// TODO: elevation_min data are missing during load_tiles in a grid
+struct terrain_grid {
+	/* TODO: should be read_tiles member of terrain_grid? I think in the first step it is easier to
+	implement due to undestricted access and as a second step we can make it non member funnction if
+	it makes any sence. */
+
+	void load_tiles();  // TODO: check that elevation tiles are all the same (width, height), the same for satellite tiles
+	[[nodiscard]] size_t size() const {return std::size(_terrains);}
+
+	/*! \returns Range to iterate through list of terrains.
+	\code
+	terrain_grid terrains;
+	// ...
+	for (terrain const & terrains.iterate()) {...}
+	\endcode */
+	auto iterate() const {  //!< \returns list of terrains as range
+		return std::ranges::subrange{std::begin(_terrains), std::end(_terrains)};
+	}
+
+	// TODO: some basic tile informations
+	int elevation_tile_size;  //= 716, TODO: this is set during load_tiles()
+
+	int grid_column_count = 2;
+	float quad_size = 1.0f;
+
+	/* TODO: This is how wee work with elevations in a vertx shader program
+	float h = float(texture(heights, position.xy).r) * elevation_scale * height_scale; */
+	constexpr static int elevation_tile_max_value[4] = {
+		564, 726,
+		625, 805
+	};
+
+	~terrain_grid() {
+		for (terrain const & trn : _terrains) {  // TODO: terrain is now owner of textures so it is terrain responsibility to delete textures
+			glDeleteTextures(1, &trn.elevation_map);
+			glDeleteTextures(1, &trn.satellite_map);
+		}
+	}
+
+private:
+	std::vector<terrain> _terrains;  // TODO: we need cleanup of textures in destructor
+};
